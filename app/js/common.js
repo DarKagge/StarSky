8c61ae53e5641570b36d82ac3df3cb1e4a05280b
jeka <ElectroDeggi33759@yandex.ru>
2016-12-13 18:53:15 +0200

    1

--- app/js/common.js
+++ app/js/common.js
@@ -112,4 +112,30 @@ $(function() {
 		}
 		return false;
 	})
+
+
+
+
+
+
+
+
+
+
+	$(document).ready(function() {
+		$('.imageGallery').lightSlider({
+			gallery:true,
+			item:1,
+			vertical:true,
+			verticalHeight:295,
+			vThumbWidth:50,
+			thumbItem:8,
+			thumbMargin:4,
+			slideMargin:0,
+
+		});
+	});
+
+
+
 });
