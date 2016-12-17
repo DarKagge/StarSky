53f186bea4b693774bb111d574df11322c2283d1
jeka <ElectroDeggi33759@yandex.ru>
2016-12-17 15:24:11 +0200

    1

--- app/js/common.js
+++ app/js/common.js
@@ -27,9 +27,8 @@ $(function() {
         var ref = btn.closest('form').find('[required]');
         var msg = btn.closest('form').find('input, textarea');
         var send_btn = btn.closest('form').find('[name=send]');
-        var send_options = btn.closest('form').find('[name=campaign_token]');
 
-        $(ref).each(function () {
+        $(ref).each(function() {
             if ($(this).val() == '') {
                 var errorfield = $(this);
                 $(this).addClass('error').parent('.field').append('<div class="allert"><span>Заполните это поле</span><i class="fa fa-exclamation-triangle" aria-hidden="true"></i></div>');
@@ -39,7 +38,7 @@ $(function() {
             } else {
                 var pattern = /^([a-z0-9_\.-])+@[a-z0-9-]+\.([a-z]{2,4}\.)?[a-z]{2,4}$/i;
                 if ($(this).attr("type") == 'email') {
-                    if (!pattern.test($(this).val())) {
+                    if(!pattern.test($(this).val())) {
                         $("[name=email]").val('');
                         $(this).addClass('error').parent('.field').append('<div class="allert"><span>Укажите коректный e-mail</span><i class="fa fa-exclamation-triangle" aria-hidden="true"></i></div>');
                         error = 1;
@@ -47,8 +46,8 @@ $(function() {
                     }
                 }
                 var patterntel = /^()[0-9]{9,18}/i;
-                if ($(this).attr("type") == 'tel') {
-                    if (!patterntel.test($(this).val())) {
+                if ( $(this).attr("type") == 'tel') {
+                    if(!patterntel.test($(this).val())) {
                         $("[name=phone]").val('');
                         $(this).addClass('error').parent('.field').append('<div class="allert"><span>Укажите коректный номер телефона</span><i class="fa fa-exclamation-triangle" aria-hidden="true"></i></div>');
                         error = 1;
@@ -57,93 +56,56 @@ $(function() {
                 }
             }
         });
-        if (!(error == 1)) {
-            $(send_btn).each(function () {
+        if(!(error==1)) {
+            $(send_btn).each(function() {
                 $(this).attr('disabled', true);
             });
-            $(send_options).each(function () {
-                var form = $(this).closest('form'), name = form.find('.name').val();
-                if ($(this).val() == '') {
-                    $.ajax({
-                        type: 'POST',
-                        url: 'mail.php',
-                        data: msg,
-                        success: function () {
-                            $("#modal_callback_ok h4").remove();
-                            $("#modal_callback_ok").prepend("<h4>" + name + ",</h4>");
-                            $('form').trigger("reset");
-                            setTimeout(function () {
-                                $("[name=send]").removeAttr("disabled");
-                            }, 1000);
-                            // Настройки модального окна после удачной отправки
-                            $(".fancybox-close").click();
-                            $('div.md-show').removeClass('md-show');
-                            $("#call_ok")[0].click();
-                        },
-                        error: function (xhr, str) {
-                            alert('Возникла ошибка: ' + xhr.responseCode);
-                        }
-                    });
-                } else {
-                    $.ajax({
-                        type: 'POST',
-                        url: 'mail.php',
-                        data: msg,
-                        success: $.ajax({
-                            type: 'POST',
-                            url: 'https://app.getresponse.com/add_subscriber.html',
-                            data: msg,
-                            statusCode: {
-                                0: function () {
-                                    $("#modal_callback_ok h4").remove();
-                                    $("#modal_callback_ok").prepend("<h4>" + name + ",</h4>");
-                                    $('form').trigger("reset");
-                                    setTimeout(function () {
-                                        $("[name=send]").removeAttr("disabled");
-                                    }, 1000);
-                                    $(".fancybox-close").click();
-                                    // Настройки модального окна после удачной отправки
-                                    $('div.md-show').removeClass('md-show');
-                                    $("#call_ok")[0].click();
-                                }
-                            }
-                        }),
-                        error: function (xhr, str) {
-                            alert('Возникла ошибка: ' + xhr.responseCode);
-                        }
-                    });
+
+            var form = btn.closest('form'), name = form.find('[name=name]').val();
+
+            $.ajax({
+                type: 'POST',
+                url: '/mail.php',
+                data: msg,
+                success: function(data) {
+                    $.magnificPopup.close();
+                    $("#ThankYou span").text(name);
+                    $("a[href='#ThankYou']").click();
+                },
+                error: function(xhr, str) {
+                    alert('Возникла ошибка: ' + xhr.responseCode);
                 }
             });
         }
         return false;
-    })
-
-
-    $(document).ready(function () {
-        $('.imageGallery').lightSlider({
-            gallery: true,
-            item: 1,
-            vertical: true,
-            verticalHeight: 295,
-            vThumbWidth: 50,
-            thumbItem: 8,
-            thumbMargin: 4,
-            slideMargin: 0,
-
-        });
     });
 
 
-    $('.imgeslider').lightSlider({
+$(document).ready(function () {
+    $('.imageGallery').lightSlider({
         gallery: true,
         item: 1,
-        loop: true,
-        thumbItem: 9,
+        vertical: true,
+        verticalHeight: 295,
+        vThumbWidth: 50,
+        thumbItem: 8,
+        thumbMargin: 4,
         slideMargin: 0,
-        enableDrag: false,
-        currentPagerPosition: 'top',
 
     });
+});
+
+
+$('.imgeslider').lightSlider({
+    gallery: true,
+    item: 1,
+    loop: true,
+    thumbItem: 9,
+    slideMargin: 0,
+    enableDrag: false,
+    currentPagerPosition: 'top',
+
+});
 
 
 
@@ -152,27 +114,27 @@ $(function() {
 
 
 
-    $(".toggle_mnu").click(function() {
-        $(".sandwich").toggleClass("active");
-    });
+$(".toggle_mnu").click(function() {
+    $(".sandwich").toggleClass("active");
+});
 
-    $(".top_mnu ul a").click(function() {
-        $(".top_mnu").fadeOut(600);
-        $(".sandwich").toggleClass("active");
+$(".top_mnu ul a").click(function() {
+    $(".top_mnu").fadeOut(600);
+    $(".sandwich").toggleClass("active");
+    $(".top_text").css("opacity", "1");
+}).append("<span>");
+
+$(".toggle_mnu").click(function() {
+    if ($(".top_mnu").is(":visible")) {
         $(".top_text").css("opacity", "1");
-    }).append("<span>");
-
-    $(".toggle_mnu").click(function() {
-        if ($(".top_mnu").is(":visible")) {
-            $(".top_text").css("opacity", "1");
-            $(".top_mnu").fadeOut(600);
-            $(".top_mnu li a").removeClass("fadeInUp animated");
-        } else {
-            $(".top_text").css("opacity", ".1");
-            $(".top_mnu").fadeIn(600);
-            $(".top_mnu li a").addClass("fadeInUp animated");
-        };
-    });
+        $(".top_mnu").fadeOut(600);
+        $(".top_mnu li a").removeClass("fadeInUp animated");
+    } else {
+        $(".top_text").css("opacity", ".1");
+        $(".top_mnu").fadeIn(600);
+        $(".top_mnu li a").addClass("fadeInUp animated");
+    };
+});
 
 
 
@@ -184,21 +146,19 @@ $(function() {
 
 
 
-    $('.open-popup-link').magnificPopup({
-        type:'inline',
-        midClick: true // Allow opening popup on middle mouse click. Always set it to true if you don't provide alternative source in href.
-    });
-    $(".info-item > div:first-child").show();
-    $(".service-item .item").on("click",function () {
-        var id  = $(this).data("target");
-        $("#"+id).show().siblings("div").hide();
-    });
+$('.open-popup-link').magnificPopup({
+    type:'inline',
+    midClick: true // Allow opening popup on middle mouse click. Always set it to true if you don't provide alternative source in href.
+});
+$(".info-item > div:first-child").show();
+$(".service-item .item").on("click",function () {
+    var id  = $(this).data("target");
+    $("#"+id).show().siblings("div").hide();
+});
 
 
 
-    $("a[href*='#']").mPageScroll2id();
-
-
+$("a[href*='#']").mPageScroll2id();
 
 
 
